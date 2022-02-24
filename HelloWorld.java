6458c61fb71c7e456a25663c6291f01f30c12139
azeemkhalid22 <broodhijack123@gmail.com>
2022-02-23 22:25:38 -0800

    One java file is added

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+ 
+    public static void main(String[] args){
+
+        System.out.println("Hello GentleMan Welcome to the New world!");
+        System.out.println("Hello GentleMan Welcome to Next Semester!");
+
+    }
+
+
+
+}
